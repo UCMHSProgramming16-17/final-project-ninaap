15a60b87bb5fc231c5eab2e5dca809eeb59ef59a
Nina Paradiso <nparadiso@ucvts.org>
2017-01-23 15:39:19 +0000

    final project

--- bar.py
+++ bar.py
@@ -8,7 +8,7 @@ df = pd.read_csv('parks.csv')
 
 from bokeh.charts import Bar, output_file, save
 
-p = Bar(df, 'Park Name', color='Park Name', title="Parks By Size")
+p = Bar(df,label='Park Name', values='Acres', color='Park Name', title="Parks By Size")
 
 output_file("bar.html")
 
