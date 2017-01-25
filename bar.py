f33c6050389286c1dd327a3af96f8987553a631b
Nina Paradiso <nparadiso@ucvts.org>
2017-01-25 17:16:39 +0000

    added comments

--- bar.py
+++ bar.py
@@ -1,15 +1,21 @@
+#import necessary modules
 import csv
 
 import pandas as pd
 
 import bokeh
 
+#import/read the csv file
 df = pd.read_csv('parks.csv')
 
+#import necessary visual imports
 from bokeh.charts import Bar, output_file, save
 
+#set up bar graph visuals
 p = Bar(df,label='Park Name', values='Acres', color='Park Name', title="Parks By Size")
 
+#create the output file
 output_file("bar.html")
 
+#save bar graph to output file
 save(p)
